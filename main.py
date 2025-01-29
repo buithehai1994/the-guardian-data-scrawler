3b8ddd04a30549ad4910253c1df2c55644bdb283
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2025-01-29 18:30:04 +1100

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import pandas as pd
-from tqdm import tqdm  # Import tqdm for the progress bar
+from tqdm import tqdm  
 from datetime import datetime, timedelta
 from functions import RSSParser
 
-# List of RSS Feed URLs and their corresponding categories
 world_news = [
     ("https://www.theguardian.com/uk/rss", "UK"),
     ("https://www.theguardian.com/world/rss", "World"),
@@ -19,57 +18,40 @@ world_news = [
     ("https://www.theguardian.com/food/rss", "Food")
 ]
 
-# Create an empty DataFrame to store all the article URLs and categories
-df = pd.DataFrame(columns=["url", "category"])
-
-
 def main():
     try:
-        # Loop through each RSS feed URL and parse the data with a progress bar using tqdm
+        df_list = []  # List to store temporary DataFrames
+        
         for rss_url, category in tqdm(world_news, desc="Processing RSS Feeds", unit="feed"):
-            # Create an instance of the RSSParser for each RSS feed
             rss_parser = RSSParser(rss_url, category)
-        
-            # Fetch the RSS feed data
             rss_parser.fetch_rss_data()
-        
-            # Parse the RSS data to extract article URLs and assign categories
             rss_parser.parse_rss_data()
-        
-            # Get the articles DataFrame and append to the main DataFrame
             temp_df = rss_parser.get_articles()
-            
-            # Concatenate the temporary DataFrame with the main one
-            df = pd.concat([df, temp_df], ignore_index=True)
+            df_list.append(temp_df)
         
-        # Now df contains all article URLs and categories
-        # Apply scraping to each URL with a progress bar
+        df = pd.concat(df_list, ignore_index=True)
+
         articles_data = []
-        for index, row in tqdm(df.iterrows(), total=df.shape[0], desc="Scraping Articles", unit="article"):
-            article_info = rss_parser.scrape_url(row['url'], row['rss'], row['category'])
+        for _, row in tqdm(df.iterrows(), total=df.shape[0], desc="Scraping Articles", unit="article"):
+            rss_parser = RSSParser(row['url'], row['category'])
+            article_info = rss_parser.scrape_url(row['url'], row['category'])
             articles_data.append(article_info)
         
-        # Convert the scraped data into a DataFrame
         articles_df = pd.DataFrame(articles_data)
-        
-        # Optionally, clean up the DataFrame by dropping unnecessary columns if needed
-        articles_df = articles_df.drop(columns=['author'], errors='ignore')
+        articles_df.drop(columns=['author'], errors='ignore', inplace=True)
 
         filtered_articles = RSSParser.filter_by_date(articles_df)
-        
-        # Define date
-        yesterday = datetime.now().date() - timedelta(days=1)
 
-        # Save the filtered DataFrame as a JSON file to be pushed to GitHub
+        yesterday = datetime.now().date() - timedelta(days=1)
         file_path = f'processed_files/guardian_articles_{yesterday}.json'
-        result_dict = filtered_articles.convert_to_json(filtered_articles, file_path)
         
-        return result_dict  # Return the dictionary
+        result_dict = RSSParser.convert_to_json(filtered_articles, file_path)
         
+        return result_dict
+    
     except Exception as e:
         print(f"An error occurred: {e}")
-        return None  # Return None if an error occurs
-
+        return None
 
 if __name__ == '__main__':
     main()
